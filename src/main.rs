9ccb7999445e1e764ea983dc30a641fea9890891
cypherbeep <500068114@stu.upes.ac.in>
2022-02-13 19:57:22 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
